a0f213f22d11d453d142baa3e3f15186798fcd79
Maciulka <maciejowok@gmail.com>
2025-03-26 11:26:54 +0100

    EZZ

--- main.cpp
+++ main.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
+#include <restclient-cpp/restclient.h>
 
+int main() {
+    RestClient::Response r = RestClient::get("https://httpbin.org/get");
+
+    std::cout << "Status Code: " << r.code << "\n";
+    std::cout << "Response Body:\n" << r.body << "\n";
+
+    return 0;
+}
